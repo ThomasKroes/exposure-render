ceba2b67efcd7b25e9023b8f61c2c2d73d33f9d5
Thomas <Thomas@Workstation.home>
2011-08-31 20:24:55 +0200

    - Added sources

---
+++ Source/TransferFunctionGradient.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QtGui>
+
+class QTransferFunction;
+class QNodeItem;
+class QNode;
+
+class QTransferFunctionGradient : public QGraphicsRectItem
+{
+public:
+    QTransferFunctionGradient(QGraphicsItem* pParent, QGraphicsScene* pGraphicsScene);
+
+public:
+	void Update(void);
+
+	QPointF SceneToTransferFunction(const QPointF& ScenePoint);
+	QPointF TransferFunctionToScene(const QPointF& TfPoint);
+
+protected:
+	QList<QGraphicsRectItem*>	m_CheckerRectangles;
+	QGraphicsRectItem*			m_pGradientRectangle;
+	QLinearGradient				m_LinearGradient;
+
+	friend class QTransferFunctionView;
+	friend class QNodeItem;
+};
